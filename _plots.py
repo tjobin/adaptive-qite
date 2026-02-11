e6ec12b26c75280c1b300b6b139869fd81d5f6d5
Timothé Jobin <timoj@tsf-516-wpa-6-157.epfl.ch>
2026-02-11 10:16:24 +0100

    improved readability in _plots.py

--- _plots.py
+++ _plots.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from matplotlib.lines import Line2D
-from qiskit.quantum_info import entropy
-from qiskit.quantum_info import partial_trace, DensityMatrix, Statevector
+from qiskit.quantum_info import partial_trace, DensityMatrix, Statevector, entropy
 import numpy as np
 import matplotlib.ticker as ticker
+from qiskit import QuantumCircuit
 
 
 plt.rcParams.update({
@@ -23,14 +23,32 @@ colors=[mcolors.TABLEAU_COLORS['tab:blue'],
         mcolors.TABLEAU_COLORS['tab:brown']]
 
 def make_energy_vs_depth_plot(
-        qcs_per_type,
-        converged_energies_per_type,
-        dts,
-        fci_energy,
-        labels=['Fixed QITE', 'Adaptive QITE'],
-        markers=['o', 'x'],
-        filename='figs/default_filename.pdf'):
+        qcs_per_type: list[list[QuantumCircuit]],
+        converged_energies_per_type: list[list[float]],
+        dts: list[float],
+        fci_energy: float,
+        labels: list[str] = ['Fixed QITE', 'Adaptive QITE'],
+        markers: list[str] = ['o', 'x'],
+        filename: str ='figs/default_filename.pdf'
+        ) -> None:
     
+    """
+    Saves a figure with converged energy vs circuit depth for different quantum algorithms (e.g. Adaptive QITE vs QITE)
+    in a figs/ folder
+        Args:
+            - qcs_per_type: list, of N_types sublists, of len N_qcs_per_type, contains the quantum circuits obtained
+            at convergence for each type of ansatz
+            - converged_energies_per_type: list, of N_types sublists, of len N_qcs_per_type, contains the converged energy
+            - dts: list, of len N_qcs_per_type, contains the initial dt used for each type of ansatz (e.g. for Adaptive
+            QITE, the initial dt is the one used at the first iteration)
+            - fci_energy: float, reference energy
+            - labels: list, of N_types strings, where each string is the name of an ansatz
+            - markers: list, of N_types strings, where each string is the marker associated to an ansatz
+            - filename: str, name of the .pdf file to be saved
+        Returns:
+            Nothing
+    """
+
     dt_color_map = dict(zip(dts, colors))
 
     plt.figure()
@@ -56,137 +74,178 @@ def make_energy_vs_depth_plot(
         for j, (depth, converged_energy) in enumerate(zip(depths, converged_energies)):
             plt.plot(depth, converged_energy, markersize=8, markeredgewidth=1.5, label=labels[i], marker=markers[i], color=colors[j], linestyle=None, alpha=0.8)
     plt.legend(handles = all_handles)
-    plt.tight_layout()
-    plt.savefig('figs/' + filename + '.pdf')
+    plt.savefig('figs/' + filename + '.pdf', bbox_inches='tight')
 
-def make_convergence_plot(qc, times, energies, fci_energy, filename):
-    plt.figure()
-    plt.axhline(y=energies[-1], color=mcolors.TABLEAU_COLORS['tab:blue'], label=f'Converged energy at {energies[-1]:.5f} Ha')
-    plt.axhline(y=fci_energy, color='k', label=f'Exact FCI energy at {fci_energy:.5f} Ha')
-    plt.plot(times, energies, markersize=8, label=f'Converged at {energies[-1]:.5f} Ha in {len(energies)} steps (circuit depth : {qc.depth()})', alpha=0.5, linestyle=None, marker='o', linewidth=0)
-    plt.xlabel(r'Time ($t_h$)')
-    plt.ylabel('Energy (Ha)')
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig(f'figs/{filename}.pdf')
+def make_convergence_plots_per_param(
+        iters: list[int],
+        energies_per_type_per_param: list[list[list[float]]],
+        params: list[float],
+        param_name: str,
+        fci_energy: float,
+        labels: list[str] = ['UCCSD', 'EfficientSU2'],
+        markers: list[str] = ['o', '^'],
+        filename: str = 'default_filename.pdf'
+        ) -> None:
+    
+    """
+    Saves a figure with (1 x 2) or (2 x N_params / 2) convergence subplots for different values of a given parameter in a figs/ folder
+        Args:
+            - iters: list, contains the integers [0, 1, 2, ..., N_iters-1]
+            - energies_per_type_per_param: list, of N_params sublists, of N_types subsublists, of len N_iters, contains the energies
+            at each value of the parameter, for each type of ansatz, and at each iteration
+            - params: list, contains the different values of the parameter of interest
+            - param_name: str, is the name of the parameter of interest
+            - fci_energy: float, reference energy
+            - labels: list, of N_types strings, where each string is the name of an ansatz
+            - markers: list, of N_types strings, where each string is the marker associated to an ansatz
+            - filename: str, name of the .pdf file to be saved
+        Returns:
+            Nothing
+    """
 
-def make_pes_plot(
-        distances,
-        energies_per_type,
-        timesteps_required,
-        labels = ['AQITE energy', 'Exact FCI'],
-        markers=['o', '^'],
-        filename='default_filename.pdf'
-                  ):
-    fig, ax1 = plt.subplots()
+    if len(params) <= 2:
+        nrows = 1
+        ncols = len(params)
+    else:
+        nrows = 2  # Fixed number of rows
+        ncols = (len(params) + 1) // 2  # Calculate number of columns for 2 rows
+    fig = plt.figure(figsize=(4 * ncols, 4 * nrows))  # Adjust figure size for m x n grid
+    gs = fig.add_gridspec(nrows, ncols, wspace=0.1, hspace=0.3)  # m x n grid with spacing
+    axs = gs.subplots(sharex=True, sharey=True)
 
-    # 2. Create the second axis (Time Steps) sharing the same X-axis
-    ax2 = ax1.twinx()
-    ax1.set_xlabel('Bond distance [Å]')
-    ax1.set_ylabel('Energy')
-    ax2.set_ylabel('Time steps')
-    ax1.tick_params(axis='y')
-    ax2.tick_params(axis='y')
-    for i, energies in enumerate(energies_per_type):
-        if i == 0:
-            ax1.plot(distances, energies, label=labels[i], marker=markers[i], alpha=0.7, markersize=8, markeredgewidth=1.5,linestyle=None, linewidth=0, color=mcolors.TABLEAU_COLORS['tab:blue'])
-            ax2.plot(distances,  timesteps_required, label='Time steps', marker='^', alpha=0.7, markersize=8, markeredgewidth=1.5,linestyle=None, linewidth=0, color=mcolors.TABLEAU_COLORS['tab:green'])
+    for i, param in enumerate(params):
+        if len(params) <= 2:
+            row = 0
+            col = i
+            ax = axs[i]
         else:
-            ax1.plot(distances, energies, label=labels[i], marker=markers[i], alpha=0.7, markersize=0, markeredgewidth=0,linestyle='--', color='k')
+            row, col = divmod(i, ncols)  # Determine row and column for the grid
+            ax = axs[row, col] if col > 1 else axs[row]  # Access subplot based on row and column
+        ax.set_title(rf'{param_name} = {param}')  # Add title for each subplot
+        ax.set_xlabel('Iterations')
+        if col == 0:  # Add y-axis label only for the first column
+            ax.set_ylabel('Energy (Ha)')
 
+        for j, energies in enumerate(energies_per_type_per_param[i]):
+            ax.plot(
+                np.concatenate([iters, [len(iters),]])[::5], # plots every 5th iteration
+                energies[50::3*5], # assuming 3 circuit evaluations per optimization step (SPSA with blocking)
+                markersize=8,
+                label=labels[j],
+                alpha=0.7,
+                linestyle=None,
+                marker=markers[j],
+                linewidth=0,
+                color=colors[j]
+                )
+        ax.axhline(y=fci_energy, color='k', label=f'Exact FCI energy', linestyle='--')
 
-    # Combine them into one legend
-    handles1, labels1 = ax1.get_legend_handles_labels()
-    handles2, labels2 = ax2.get_legend_handles_labels()
-    handles, labels = [handles1[0]] + handles2 + [handles1[1]], [labels1[0]] + labels2 + [labels1[1]]
-    fig.legend(handles, labels, loc='lower center', ncol=len(labels), bbox_to_anchor=(0.5, -0.05))    
-    fig.tight_layout()
+    # Create a common legend
+    handles, labels = axs.flat[-1].get_legend_handles_labels() if len(params) > 1 else axs.flat[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=len(labels), bbox_to_anchor=(0.5, -0.15))
     plt.savefig(f'figs/{filename}.pdf', bbox_inches='tight')
-    plt.close(fig)
+
+def make_pes_plots_per_param(
+        distances: list[float],
+        energies_per_type_per_param: list[list[list[float]]], 
+        params: list[float],
+        param_name: str,
+        fci_energies: list[float],
+        labels: list[str] = ['UCCSD', 'EfficientSU2'],
+        markers: list[str] = ['o', '^'],
+        filename: str = 'default_filename.pdf'
+        ) -> None:
+    
+    """
+    Saves a figure with 1 x N_params potential energy surface subplots for N_types different ansatze
+    for different values of a given parameter in a figs/ folder
+        Args:
+            - distances: list, of N_dist float, contains each bond length at which the PES is evaluated
+            - energies_per_type_per_param: list, of N_params sublists, of N_types subsublists, contains the energies at 
+            each value of the parameter for each type of ansatz at each bond length
+            - params: list, of N_params numbers, contains the different values of the parameter of interest
+            - param_name: str, is the name of the parameter of interest
+            - fci_energies: list, of N_dist numbers, contains the reference energy at each bond length
+            - labels: list, of N_types strings, where each string is the name of an ansatz
+            - markers: list, of N_types strings, where each string is the marker associated to an ansatz
+            - filename: str, name of the .pdf file to be saved
+        Returns:
+            Nothing
+    """
+
+    fig = plt.figure(figsize=(3 * len(params), 4))
+    gs = fig.add_gridspec(1, len(params), wspace=0)
+    axs = gs.subplots(sharex=True,sharey=True)
+    # fig, axs = plt.subplots(1, len(nshots_list), figsize=(6 * len(nshots_list), 4), sharey=True)  # Horizontal layout with shared y-axis
+    for i, param in enumerate(params):
+        energies_per_type = [energies_per_type_per_param[i][j] for j in range(len(energies_per_type_per_param[i]))]
+        ax = axs[i] if len(params) > 1 else axs  # Handle case where nshots_list has only one element
+        ax.set_title(f'{param_name} =  {param}')  # Add title for each subplot
+        ax.set_xlabel('Bond distance [Å]')
+        if i == 0:
+            ax.set_ylabel('Energy [Ha]')
+
+        for j, energies in enumerate(energies_per_type):
+            ax.plot(distances, energies, label=labels[j], marker=markers[j], alpha=0.7, markersize=8, markeredgewidth=1.5, linestyle=None, linewidth=0, color=colors[j])
+        ax.plot(distances, fci_energies, label='Exact FCI', alpha=0.7, markersize=0, markeredgewidth=0, linestyle='--', color='k')
+
+    # Create a common legend
+    handles, labels = axs[-1].get_legend_handles_labels() if len(params) > 1 else axs.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=len(labels), bbox_to_anchor=(0.5, -0.15))
+    plt.subplots_adjust(wspace=0)  # No horizontal space between subplots
+    plt.tight_layout()
+    plt.savefig(f'figs/{filename}.pdf', bbox_inches='tight')
 
 def make_entropy_plot(
-        distances,
-        qcs,
-        filename
-):
+        distances: list[float],
+        qcs: list[QuantumCircuit],
+        filename: str = 'default_filename.pdf'
+    ) -> None:
+
+    """
+    Saves a figure with a single subplot of the entanglement entropy of qubit 0 and 2 at N_dist
+    different bond distances in a figs/ folder
+        Args:
+            - distances: list, of len N_dist, contains the bond lengths
+            - qcs: list, of len N_dist, contains the converged states at the different bond lengths
+            - filename: str, name of the .pdf file to be saved
+        Returns:
+            Nothing
+    """  
+
     states = [Statevector(qc) for qc in qcs]
     rhos_q0 = [partial_trace(state, [1,3]) for state in states]
     entropies = [entropy(rho) for rho in rhos_q0]
     fig, ax = plt.subplots()
-    ax.plot(distances, entropies, marker='o', label='AQITE entanglement entropy', alpha=0.7, markersize=8, markeredgewidth=1.5,linestyle='-.', color='sandybrown')
-    # ax.axhline(0.0313, color='k', label='Exact, He')
+    ax.plot(distances, entropies, marker='o', label=r'Adaptive QITE', alpha=0.7, markersize=8, markeredgewidth=1.5,linestyle='-.')
     ax.set_ylabel('Entanglement entropy')
-    ax.set_xlabel('Bond distance')
+    ax.set_xlabel('Bond distance [Å]')
     ax.legend(loc='best')
-    fig.tight_layout()
-    plt.savefig(f'figs/{filename}.pdf')
-    plt.close(fig)
-
-def make_energy_dt_vs_iter_plot(
-        energies_per_initdt,
-        dts_per_initdt,
-        init_dts,
-        fci_energy,
-        filename
-):
-    fig = plt.figure(figsize=(2 * len(dts_per_initdt), 4))
-    gs = fig.add_gridspec(1, 4, wspace=0)
-    (ax1, ax3, ax5, ax7) = gs.subplots(sharex=False,sharey=True)
-    ax2 = ax1.twinx()
-    ax4 = ax3.twinx()
-    ax6 = ax5.twinx()
-    ax8 = ax7.twinx()
-
-    prim_ax = [ax1,ax3,ax5, ax7]
-    second_ax = [ax2, ax4, ax6, ax8]
-    for i, ax in enumerate(prim_ax):
-        ax.set_title(r'$\Delta\tau^{(0)}$' + rf' = $10^{{{-i-1}}}$')
-        ax2_p = second_ax[i]
-        if i == 0:
-            ax2_p.set_ylim(bottom=-0.05, top=0.6)
-        ax.set_xlabel('Time steps', color='k')
-        ax.set_ylabel('Energy [Ha]', color='tab:blue')
-        ax2_p.set_ylabel(r'$^\Delta\tau^{(i)}$' + r' [$\hbar$/Ha]', color='tab:orange')
-        ax.tick_params(axis='y', labelcolor='tab:blue')
-        ax2_p.tick_params(axis='y', labelcolor='tab:orange')
-        iters = np.arange(len(energies_per_initdt[i][0]))
-        print(iters)
-        
-        ax.plot(iters, energies_per_initdt[i][0], marker='o', alpha=0.7, markersize=8, linestyle=None, linewidth=0, color=colors[0])
-        ax2_p.plot(iters,  dts_per_initdt[i][0], marker='x', alpha=0.7, markersize=8, markeredgewidth=1.5,linestyle=None, linewidth=0, color=colors[1])
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-        ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
-        if i == 0:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
-            ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
-        else:
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(4))
-            ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
-        # elif i==2:
-        #     ax.xaxis.set_major_locator(ticker.MultipleLocator(4))
-        #     ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
-         
-        ax.axhline(fci_energy, linestyle='--', color='k', label='Exact energy')
-        # h1, l1 = ax.get_legend_handles_labels()
-        # # Plot on axis 2
-        # h2, l2 = ax2.get_legend_handles_labels()
-        # ax.legend(h1 + h2, l1 + l2, loc='lower right')
-
-    ax4.sharey(ax2)
-    ax6.sharey(ax4)
-    ax8.sharey(ax6)
-    # ax2.sharey(ax6)
-    fig.tight_layout()
-    plt.savefig(f'figs/{filename}.pdf')
+    plt.savefig(f'figs/{filename}.pdf', bbox_inches='tight')
     plt.close(fig)
 
 def make_energy_dt_vs_iter_plot_general(
-        energies_per_initdt,
-        dts_per_initdt,
-        init_dts,
-        fci_energy,
-        filename
-):
+        energies_per_initdt: list[list[float]],
+        dts_per_initdt: list[list[float]],
+        init_dts: list[float],
+        fci_energy: float,
+        filename: str = 'default_filename'
+) -> None:
+    
+    """
+    Saves a figure with N subplots of energy and time intervals vs iterations for N different initial dts
+    in a figs/ folder
+        Args:
+            - energies_per_initdt: list, of N sublists, of len N_iters, contains the energies at each iteration
+            for each initial dt
+            - dts_per_initdt: list, of N sublists, of len N_iters, contains the time intervals at each iteration
+            - init_dts: list, of N floats, contains the different initial dts
+            - fci_energy: float, reference energy
+            - filename: str, name of the .pdf file to be saved
+        Returns:
+            Nothing
+    """
+    
     num_dts = len(init_dts)  # Determine the number of initial dts
     num_rows = 2  # Always use 2 rows
     num_cols = (num_dts + num_rows - 1) // num_rows  # Calculate columns needed
